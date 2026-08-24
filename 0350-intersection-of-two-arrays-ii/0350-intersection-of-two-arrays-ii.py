53ac43a4046a3ce272a2d177336392342e201cb7
Vishal Kumar <993munna993@gmail.com>
2026-08-24 18:43:28 +0530

    Time: 0 ms (100%), Space: 12.5 MB (64.74%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        dic = {}
+        arr = []
+
+        for i in nums1:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+
+        for i in nums2:
+            if i in dic and dic[i] > 0:
+                arr.append(i)
+                dic[i] -= 1
+
+        return arr
